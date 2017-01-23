d0a46af7efe5d5978c73c76f7c930e40c2acfc4d
jermbo <jermbo@cosmicstrawberry.com>
2017-01-23 17:11:01 -0500

    Added basic functionality for knowing what row you are in, selecting the item and adding the value to a matrix. Now I am tring to figure out how to know if you got 5 in a row

--- gulpfile.js
+++ gulpfile.js
@@ -73,7 +73,7 @@ gulp.task('dev-images', () => {
 });
 
 gulp.task('dev-watch', ['dev-server'], () => {
-  gulp.watch(config.styles.source + '/**/*.less', ['dev-styles']);
+  gulp.watch(config.styles.source, ['dev-styles']);
   gulp.watch(config.html.source, ['dev-html', 'dev-bsReload']);
   gulp.watch(config.scripts.source, ['dev-scripts', 'dev-bsReload']);
   gulp.watch(config.images.source, ['images', 'bsReload']);
